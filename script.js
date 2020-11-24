3f9deba1acb4dc965fda5ec59e5530f99050330f
krishnakant48 <71984514+krishnakant48@users.noreply.github.com>
2020-11-24 13:08:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,4 +18,9 @@
     { 
         document.getElementById("result").value = "" 
     } 
+    function back() {
+        var value = document.getElementById("result").value;
+        document.getElementById("result").value = value.substr(0, value.length - 1);
+    }
+             
   
